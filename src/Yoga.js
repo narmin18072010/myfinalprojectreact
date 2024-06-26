b4543f08fa755377561a3af079a7df5e56ce99c1
narmin18072010 <narminalieva2010@mail.ru>
2024-06-26 14:07:09 +0300

    First commit

--- src/Yoga.js
+++ src/Yoga.js
@@ -1,12 +1,9 @@
 import {
      Link
 } from "react-router-dom";
-import { useAuth0 } from '@auth0/auth0-react'; 
 import { video } from "./data"
 
 function Yoga() {
-     const {isLoading} = useAuth0 
-     if (isLoading) return <h1>Loading...</h1>
 return (
      <div className="containerBigFitness">
      <div>
